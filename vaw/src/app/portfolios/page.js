91072f002b2870bbb3c58c053c644678c164e5a2
eigenomar <omarkhaledsamy20@gmail.com>
2024-10-05 10:43:38 +0200

    fix issue with paths

--- vaw/src/app/portfolios/page.js
+++ vaw/src/app/portfolios/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import PortfolioForm from "../components/PortfolioForm";
+import PortfolioForm from "../components/portfolios/PortfolioForm";
 import Link from "next/link";
 
 const PortfoliosPage = () => {
